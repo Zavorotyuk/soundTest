e9bf95de10921cf042f23bffc17923f5546e8495
Volodymyr Zavorotyuk <v.zavorotyuk@gmail.com>
2017-08-22 18:06:47 +0300

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,16 @@ import {DomSanitizer,SafeResourceUrl,} from '@angular/platform-browser';
 })
 export class AppComponent {
 
-  private config: object = {
-    token: 'Q54HecQsdchtw3QvtsaFdBWQyYY3ehZD',
-    clientSecret: 'CCN5RVw5sYHdGVf6sh8X9WFUBb8j45rW',
-    clientId: 'g4PaKPJKaHMraXfd7a'
-  }
-
   private cloudcasts = [];
+  private soundcloudcasts = [];
+  private user = null;
 
   constructor(private _data: DataService, public sanitizer:DomSanitizer) {}
 
   ngOnInit() {
     this.getCloudcasts();
+    this.getSoundCloudcasts();
+    this.getUserInfo();
   }
 
   getCloudcasts() {
@@ -31,8 +29,27 @@ export class AppComponent {
     );
   };
 
-  getFrameUrl(key) {
+  getMiCloudFrameUrl(key) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com${key};hide_cover=1&amp;light=1`);
+  };
+
+  getSoundCloudcasts() {
+    this._data.getSoundCloudcasts().subscribe(
+      data => this.soundcloudcasts = data,
+      err => console.log('err soundcloud', err)
+    );
+  };
+
+  getSoundCloudcastsFrameUrl(key) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${key}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true`);
+
+  }
+
+  getUserInfo() {
+    this._data.getUserInfo().subscribe(
+      data => this.user = data,
+      err => console.log('err getting user', err)
+    )
   }
 
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,13 +7,26 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class DataService {
 
-  private baseUrl: string = 'https://api.mixcloud.com';
+  private baseUrls = {
+    miCloud: 'https://api.mixcloud.com',
+    soundCloud: 'https://api.soundcloud.com'
+  }
 
   constructor(
     private _http: Http) { }
 
   getCloudcasts(): Observable<any> {
-    return this._http.get(`${this.baseUrl}/dmitriy-andreev2/cloudcasts/`)
+    return this._http.get(`${this.baseUrls.miCloud}/dmitriy-andreev2/cloudcasts/`)
+    .map(res => res.json());
+  };
+
+  getSoundCloudcasts(): Observable<any> {
+    return this._http.get(`${this.baseUrls.soundCloud}/users/327666017/tracks?client_id=69efe52021a5d2649a8ee04165867e82`)
+    .map(res => res.json());
+  };
+
+  getUserInfo(): Observable<any> {
+    return this._http.get(`${this.baseUrls.miCloud}/dmitriy-andreev2/`)
     .map(res => res.json());
   }
 
